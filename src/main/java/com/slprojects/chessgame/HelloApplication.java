8f04d66d8fec2e576a490da6f2b9f797fc95c4f3
SofianeLasri <alasri250@gmail.com>
2022-10-11 12:14:42 +0200

    Test witg bootstrap fx.

--- src/main/java/com/slprojects/chessgame/HelloApplication.java
+++ src/main/java/com/slprojects/chessgame/HelloApplication.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.kordamp.bootstrapfx.BootstrapFX;
+import org.kordamp.bootstrapfx.scene.layout.Panel;
 
 import java.io.IOException;
 
@@ -12,6 +14,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
