5e18458848492cfd923f552c749898a237c401a7
Sofiane Lasri <alasri250@gmail.com>
2022-11-29 09:42:23 +0100

    Reformatage du code.

--- chess/Piece.java
+++ chess/Piece.java
@@ -3,26 +3,29 @@ package chess;
 import java.awt.image.BufferedImage;
 
 public class Piece {
-	PieceType type;
-	BufferedImage image;
-	
-	public PieceType getType() {
-		return type;
-	}
-	public void setType(PieceType type) {
-		this.type = type;
-	}
-	public BufferedImage getImage() {
-		return image;
-	}
-	public void setImage(BufferedImage image) {
-		this.image = image;
-	}
-	
-	public Piece(String image) {
-		super();
-		this.type = new PieceType(image);
-		this.image = ChessGraphicTool.load(ChessDemo.imagePath + image);
-	}
+    PieceType type;
+    BufferedImage image;
+
+    public PieceType getType() {
+        return type;
+    }
+
+    public void setType(PieceType type) {
+        this.type = type;
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public void setImage(BufferedImage image) {
+        this.image = image;
+    }
+
+    public Piece(String image) {
+        super();
+        this.type = new PieceType(image);
+        this.image = ChessGraphicTool.load(ChessDemo.imagePath + image);
+    }
 }
 
--- chess/PieceType.java
+++ chess/PieceType.java
@@ -1,57 +1,56 @@
 package chess;
 
 public class PieceType {
-	String type; // 0 = pawn, 1 = tower, 2 = knight, 3 = fool, 4 = queen, 5 = king
+    String type; // 0 = pawn, 1 = tower, 2 = knight, 3 = fool, 4 = queen, 5 = king
 
-	public String getType() {
-		return type;
-	}
-	
-	public PieceType(String type) {
-		super();
-		this.type = type;
-	}
+    public String getType() {
+        return type;
+    }
 
-	public void setType(String type) {
-		this.type = type;
-	}
+    public PieceType(String type) {
+        super();
+        this.type = type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 
 
-
-	public int[] PossibleMove() {
-		int[] moves = {0,0,0};
-		switch(this.type) {
-			case "pawn":
-				moves[0] = 0;
-				moves[1] = 2;
-				moves[2] = 0;
-				break;
-			case "tower":
-				moves[0] = 8;
-				moves[1] = 8;
-				moves[2] = 0;
-				break;
-			case "knight":
-				moves[0] = 2;
-				moves[1] = 1;
-				moves[2] = 0;
-				break;
-			case "rook":
-				moves[0] = 0;
-				moves[1] = 0;
-				moves[2] = 8;
-				break;
-			case "queen":
-				moves[0] = 8;
-				moves[1] = 8;
-				moves[2] = 8;
-				break;
-			case "king":
-				moves[0] = 1;
-				moves[1] = 1;
-				moves[2] = 1;
-				break;
-		}
-		return moves;
-	}
+    public int[] PossibleMove() {
+        int[] moves = {0, 0, 0};
+        switch (this.type) {
+            case "pawn":
+                moves[0] = 0;
+                moves[1] = 2;
+                moves[2] = 0;
+                break;
+            case "tower":
+                moves[0] = 8;
+                moves[1] = 8;
+                moves[2] = 0;
+                break;
+            case "knight":
+                moves[0] = 2;
+                moves[1] = 1;
+                moves[2] = 0;
+                break;
+            case "rook":
+                moves[0] = 0;
+                moves[1] = 0;
+                moves[2] = 8;
+                break;
+            case "queen":
+                moves[0] = 8;
+                moves[1] = 8;
+                moves[2] = 8;
+                break;
+            case "king":
+                moves[0] = 1;
+                moves[1] = 1;
+                moves[2] = 1;
+                break;
+        }
+        return moves;
+    }
 }
