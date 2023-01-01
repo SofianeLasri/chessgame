ad70ca085e302612e29775ba83e5d6717432521b
Sofiane Lasri <alasri250@gmail.com>
2023-01-01 11:57:35 +0100

    Ajout d'une fonction permettant de mettre en evidences une cellule du tableau. Ajustement sur la fonction drawCell pour utiliser les bons numéros de cellules.

--- chess/ChessTable.java
+++ chess/ChessTable.java
@@ -7,19 +7,21 @@ import static java.lang.Math.round;
 
 public class ChessTable {
     private final int tableCellNumber = 8;
-    private final int tableCellWidth = 80;
+    private final int cellWidth = 80;
+    private final int cellHighlightStrokeWidth = round(cellWidth / 16);
     private int xOrigin;
     private int yOrigin;
     private LayerManagement chessGameLayers;
     private BufferedImage chessBoard;
     private Graphics2D chessBoardGC;
     private BufferedImage cellsHighlight;
+    private Graphics2D cellsHighlightGC;
 
     private static int positions(int x) {
         return x * 80 + 40;
     }
 
-    public ChessTable(LayerManagement mgrLayers){
+    public ChessTable(LayerManagement mgrLayers) {
         chessGameLayers = mgrLayers;
 
         chessBoard = new BufferedImage(ChessDemo.windowWidth, ChessDemo.windowHeight, BufferedImage.TYPE_INT_ARGB);
@@ -27,52 +29,55 @@ public class ChessTable {
         chessBoardGC = (Graphics2D) chessBoard.getGraphics();
         chessBoardGC.setRenderingHints(ChessDemo.rh);
 
-        // On calcule l'origine de la table d'échec, basé sur le nombre de cellules et de leur taille
-        xOrigin = round((ChessDemo.windowWidth - (tableCellNumber * tableCellWidth)) / 2);
-        yOrigin = round((ChessDemo.windowHeight - (tableCellNumber * tableCellWidth)) / 2);
+        cellsHighlight = new BufferedImage(ChessDemo.windowWidth, ChessDemo.windowHeight, BufferedImage.TYPE_INT_ARGB);
+        mgrLayers.addLayer(cellsHighlight);
+        cellsHighlightGC = (Graphics2D) cellsHighlight.getGraphics();
+        cellsHighlightGC.setRenderingHints(ChessDemo.rh);
 
-        if(xOrigin < 0 || yOrigin < 0){
+        // On calcule l'origine de la table d'échec, basé sur le nombre de cellules et de leur taille
+        xOrigin = round((ChessDemo.windowWidth - (tableCellNumber * cellWidth)) / 2);
+        yOrigin = round((ChessDemo.windowHeight - (tableCellNumber * cellWidth)) / 2);
+
+        if (xOrigin < 0 || yOrigin < 0) {
             System.out.println("Error: The jFrame (window) is not wide enought!");
-            System.out.println("Here is the minimal size: square" + (tableCellNumber * tableCellWidth) + "px");
+            System.out.println("Here is the minimal size: square" + (tableCellNumber * cellWidth) + "px");
         }
 
-        for (int x=0; x<tableCellNumber; x++){
-            for(int y=0; y<tableCellNumber; y++){
-                if((x+y) % 2 == 0){
+        for (int x = 1; x <= tableCellNumber; x++) {
+            for (int y = 1; y <= tableCellNumber; y++) {
+                if ((x + y) % 2 == 0) {
                     drawCell(x, y, Color.BLACK);
-                }else{
+                } else {
                     drawCell(x, y, Color.WHITE);
                 }
             }
         }
 
-        /*boolean pair_impair = false; // v?rifie si la case est paire ou impaire pour savoir comment la colorier
-        int line = 0; // v?rifie sur quelle ligne on est pour le d?calage des couleurs
-        for (int y = 100; y < 740; y += 80) { // lines
-            for (int x = 100; x < 740; x += 80) { // columns
-                // chessBoardGC.draw3DRect(x,y,80,80,true); // xcenter, ycenter, windowWidth=80 , windowHeight=80
-                if (pair_impair) {
-                    pair_impair = false;
-                    chessBoardGC.setColor(java.awt.Color.WHITE); // remplis la couleur du rectangle dessin?
-                } else {
-                    pair_impair = true;
-                    chessBoardGC.setColor(java.awt.Color.BLACK); // remplis la couleur du rectangle dessin?
-                }
-                line++;
-                if (line == 8) {
-                    pair_impair = !pair_impair;
-                    line = 0;
-                }
-                chessBoardGC.fillRect(x, y, 80, 80);
-            }
-        }*/
+        highLightCell(3, 3, Color.ORANGE);
     }
 
-    public void drawCell(int cellColumnNum, int cellRowNum, Color color){
-        int cellPosX = xOrigin + (cellColumnNum * tableCellWidth);
-        int cellPosY = yOrigin + (cellRowNum * tableCellWidth);
-        chessBoardGC.draw3DRect(cellPosX,cellPosY,tableCellWidth,tableCellWidth,true);
+    public void drawCell(int cellColumnNum, int cellRowNum, Color color) {
+        int cellPosX = xOrigin + ((cellColumnNum-1) * cellWidth);
+        int cellPosY = yOrigin + ((cellRowNum-1) * cellWidth);
+
+        chessBoardGC.draw3DRect(cellPosX, cellPosY, cellWidth, cellWidth, true);
         chessBoardGC.setColor(color);
-        chessBoardGC.fillRect(cellPosX,cellPosY,tableCellWidth,tableCellWidth);
+        chessBoardGC.fillRect(cellPosX, cellPosY, cellWidth, cellWidth);
+    }
+
+    public void highLightCell(int cellColumnNum, int cellRowNum, Color color) {
+        int cellPosX = xOrigin + ((cellColumnNum-1) * cellWidth);
+        int cellPosY = yOrigin + ((cellRowNum-1) * cellWidth);
+
+        cellsHighlightGC.setColor(color);
+        cellsHighlightGC.setStroke(
+                new BasicStroke(cellHighlightStrokeWidth)
+        );
+        cellsHighlightGC.drawRect(
+                cellPosX + (cellHighlightStrokeWidth / 2),
+                cellPosY + (cellHighlightStrokeWidth / 2),
+                cellWidth - (cellHighlightStrokeWidth),
+                cellWidth - (cellHighlightStrokeWidth)
+        );
     }
 }
